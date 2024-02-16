9b4bc37a05147e66ab8cc30b1cf791e525b9dbc5
Jagadeesh-Jetti <jagadeeshjetti007@gmail.com>
2024-02-16 18:58:55 +0530

    feat: connected mongoose database

---
+++ db.js
@@ -0,0 +1,25 @@
+const mongoose = require("mongoose");
+const dotENV = require("dotenv");
+
+dotEnv.config({
+  path: ".env",
+});
+
+const mongoURI = process.env.MONGODB;
+
+const initializeDatabase = () => {
+  if (!mongoURI) {
+    console.error("Environments variables not defined");
+  } else {
+    mongoose
+      .connect(mongoURI)
+      .then(() => {
+        console.log("Database connected");
+      })
+      .catch((error) => {
+        console.error("Error while connecting database,", error);
+      });
+  }
+};
+
+module.exports = initializeDatabase;
